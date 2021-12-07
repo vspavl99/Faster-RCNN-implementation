9f25a5ef3195d6d01aa7b2c2d427644b88d9043a
vpavlishen <v.pavlishen@businessdecision.ru>
2021-12-07 22:01:33 +0300

    FastRCN added

--- model/FastRCNN.py
+++ model/FastRCNN.py
@@ -4,11 +4,13 @@ from torchvision.ops import RoIPool
 from torch import Tensor
 from utils.bbox_matcher import BBoxMatcher
 from utils.sampler import Sampler
+from utils.bbox import get_proposals_from_bbox_regression, get_target_shift
+from torch.nn import functional as F
 
 
 class FastRCNN(nn.Module):
-    def __init__(self):
-        super(FastRCNN, self).__init__()
+    def __init__(self, stage: str = 'train'):
+        super().__init__()
 
         self.n_class = 20
 
@@ -30,29 +32,111 @@ class FastRCNN(nn.Module):
         self.bbox_matcher = BBoxMatcher()
         self.sampler = Sampler()
 
-    def forward(self, feature_map: Tensor, regions_of_interest: list[Tensor], targets: list[dict]):
+        self.training_stage = True if stage == 'train' else False
 
-        gt_boxes = [t["boxes"] for t in targets]
-        gt_labels = [t["labels"] for t in targets]
+    def forward(self, feature_map: Tensor, proposals: list[Tensor], targets: list[dict]):
+        labels = None
+        regression_targets = None
 
-        # append ground-truth bboxes to proposals
-        proposals = [
-            torch.cat((proposal, gt_box))
-            for proposal, gt_box in zip(regions_of_interest, gt_boxes)
-        ]
+        if self.training:
+            gt_boxes = [t["boxes"] for t in targets]
+            gt_labels = [t["labels"] for t in targets]
 
-        batch_labels, batch_matched_indexes = self.assign_targets_to_anchors(proposals, gt_boxes, gt_labels)
+            # append ground-truth bboxes to proposals
+            proposals = [
+                torch.cat((proposal, gt_box))
+                for proposal, gt_box in zip(proposals, gt_boxes)
+            ]
 
-        _, _, sampled_inds = self.sampler.create_minibatch(batch_labels)
+            batch_labels, batch_matched_indexes = self.assign_targets_to_anchors(proposals, gt_boxes, gt_labels)
+
+            labels, proposals, matched_gt_bboxes, matched_indexes = self.choose_samples(
+                batch_labels, proposals, gt_boxes, batch_matched_indexes)
+
+            regression_targets = get_target_shift(matched_gt_bboxes, proposals)
 
         # feature_map (Tensor[N, C, H, W])
-        features = self.roi_pooling(feature_map, regions_of_interest)
-
+        features = self.roi_pooling(feature_map, proposals)
         features = self.fc(features)
+        features_class, features_scores = self.fc_objects(features),  self.fc_scores(features)
 
-        # features_class, features_scores = self.fc_objects(features),  self.fc_scores(features)
-        features_class, features_scores = None, None
-        return features_class, features_scores
+        result: list[dict[str, Tensor]] = []
+        losses = {}
+        if self.training:
+            assert labels is not None and regression_targets is not None
+            loss_classifier, loss_box_reg = self.loss(features_class, features_scores, labels, regression_targets)
+            losses = {"loss_classifier": loss_classifier, "loss_box_reg": loss_box_reg}
+
+        else:
+
+            boxes, scores, labels = self.postprocess_detections(features_class, features_scores, proposals, image_shapes)
+            for boxes_per_image, scores_per_image, labels_per_image in zip(boxes, scores, labels):
+                result.append({"boxes": boxes_per_image, "labels": scores_per_image, "scores": labels_per_image})
+
+        return result, losses
+
+    def loss(self, features_class: Tensor, features_scores: Tensor, batch_labels: list[Tensor],
+             regression_targets: Tensor) -> tuple:
+        """
+
+        :param features_class:
+        :param features_scores:
+        :param batch_labels:
+        :param regression_targets:
+        :return:
+        """
+
+        labels = torch.cat(batch_labels, dim=0)
+        regression_targets = regression_targets.reshape(-1, 4)
+
+        classification_loss = F.cross_entropy(features_class, labels)
+
+        positive_indexes = torch.where(labels > 0)[0]
+        labels_pos = labels[positive_indexes]
+        features_scores = features_scores.reshape(features_scores.shape[0], -1, 4)
+
+        regression_targets_positive = regression_targets[positive_indexes]
+        # features_scores
+
+        regression_loss = F.smooth_l1_loss(
+            features_scores[positive_indexes, labels_pos],
+            regression_targets_positive,
+            reduction='mean'
+        )
+
+        return classification_loss, regression_loss
+
+    def choose_samples(self, batch_labels: list[Tensor], batch_proposals: list[Tensor],
+                       batch_gt_boxes: list[Tensor], batch_matched_indexes: list[Tensor]) -> tuple:
+        """
+        Select minibatch samples
+        :param batch_matched_indexes:
+        :param batch_labels:
+        :param batch_proposals:
+        :param batch_gt_boxes:
+        :return:
+        """
+
+        dtype = batch_proposals[0].dtype
+        device = batch_proposals[0].device
+
+        _, _, batch_sampled_indexes = self.sampler.create_minibatch(batch_labels)
+
+        labels, proposals, matched_gt_bboxes, matched_indexes = [], [], [], []
+
+        for sample_indexes_per_batch, labels_per_batch, gt_boxes_per_batch,\
+            proposals_per_batch, matched_indexes_per_batch in \
+                zip(batch_sampled_indexes, batch_labels, batch_gt_boxes, batch_proposals, batch_matched_indexes):
+
+            labels.append(labels_per_batch[sample_indexes_per_batch])
+            proposals.append(proposals_per_batch[sample_indexes_per_batch])
+            matched_indexes.append(matched_indexes_per_batch[sample_indexes_per_batch])
+
+            if gt_boxes_per_batch.numel() == 0:
+                gt_boxes_per_batch = torch.zeros((1, 4), dtype=dtype, device=device)
+            matched_gt_bboxes.append(gt_boxes_per_batch[matched_indexes_per_batch])
+
+        return labels, proposals, matched_gt_bboxes, matched_indexes
 
     def assign_targets_to_anchors(
             self, batch_proposals: list[Tensor], batch_ground_true_bboxes: list[Tensor],
@@ -97,22 +181,3 @@ class FastRCNN(nn.Module):
             batch_matched_indexes.append(clamped_matched_idxs_in_image)
 
         return batch_labels, batch_matched_indexes
-
-
-if __name__ == '__main__':
-    dummy_input = torch.randn((1, 3, 200, 200))
-
-    from torchvision.models import vgg16
-    from model.rpn import RPN
-    rpn = RPN(stage='val')
-    feature_map, filtered_proposals, loss_object_score, loss_proposals = rpn(dummy_input)
-
-    fast_rcnn = FastRCNN()
-    targets = [
-        {'boxes': torch.tensor([[50, 50, 150, 150]]), 'labels': torch.tensor([0])},
-        # {'boxes': torch.tensor([[50, 50, 150, 150]]), 'labels': torch.tensor([0])}
-        ]
-    output = fast_rcnn(feature_map, filtered_proposals, targets)
-
-    print(dummy_input.shape)
-
